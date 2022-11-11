cac6f0fd48fda26cd9c54d916403c5e5accec088
berkay çetinkaya <berkayfastsoft@gmail.com>
2022-11-11 16:50:45 +0300

    11/11/22 4:50pm

---
+++ Core/Utilities/Security/Encryption/SecurityKeyHelper.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities.Security.Encryption
+{
+
+    // işin içinde şifreleme olan her sistemde her şeyi byte array formatında veriyor olmamız lazım  
+    public class SecurityKeyHelper
+    {
+
+        public static SecurityKey CreateSecurityKey(string securityKey)
+        { 
+            return new SymmetricSecurityKey(Encoding.UTF8.GetBytes(securityKey));   
+        }
+    }
+}
---
+++ Core/Utilities/Security/Hashing/HashingHelper.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities.Security.Hashing
+{
+    public class HashingHelper
+    {
+
+        // out 
+        public static void CreatePasswordHash(string password,out byte[] passwordHash,out byte[] passwordSalt)
+        {
+
+            using (var hmac = new System.Security.Cryptography.HMACSHA512()) // class oldugu ıcın hmac ı newlıyoruz ()
+            {
+                // hmac key ile salt artı güvenlik kodu olusturuyoruz
+                passwordSalt = hmac.Key;
+                // hash byte ile veriliyor byte bulmak için utf8 encoding ile buluyoruz getbytes komutu ıle de getiriypruz.
+                passwordHash = hmac.ComputeHash((Encoding.UTF8.GetBytes(password)));
+            }
+        }
+        // sisteme gırmek ısteyen passwordun bızım hash kod ıle eşleşşip eşleşmediğini buldugumuz yer
+        public static bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512(passwordSalt)) // class oldugu ıcın hmac ı newlıyoruz ()
+            {
+               var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+                for (int i = 0; i < computedHash.Length; i++)
+                {
+                    if (computedHash[i] != passwordHash[i])
+                    {
+                        return false;
+                    }
+
+                }
+                return true;    
+            }
+
+        }
+    }
+}
