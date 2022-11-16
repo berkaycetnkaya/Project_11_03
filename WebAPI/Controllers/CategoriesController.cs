f1e725c0f8b3fe84ca8c2faf55f6faa0bf12e6ac
berkay çetinkaya <berkayfastsoft@gmail.com>
2022-11-16 17:05:39 +0300

    categoriescontroller

---
+++ WebAPI/Controllers/CategoriesController.cs
@@ -0,0 +1,36 @@
+﻿using Business.Abstract;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private ICategoryService _categoryService;
+
+        public CategoriesController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpGet("getall")]
+        public IActionResult GetList()
+        {
+            var result = _categoryService.GetAll();
+            if (result.Success)
+            {
+                return Ok(result.Data);
+            }
+
+            return BadRequest(result.Messsage);
+        }
+    }
+}
